3fbd4640469b7804a557232e1362358567c88857
Noverish Harold <embrapers263@gmail.com>
2018-01-06 16:00:26 +0900

    token to facebook_token

--- FoodFeedDoc.js
+++ FoodFeedDoc.js
@@ -19,7 +19,7 @@
  *
  * @apiParamExample {json} Request-Example:
  {
-     "token": "asdf1234",
+     "facebook_token": "asdf1234",
      "user_name": "다이어트짱"
  }
  *
@@ -35,7 +35,7 @@
  * @apiName auth_login
  * @apiGroup auth
  *
- * @apiParam {String} token Facebook 유저 id
+ * @apiParam {String} facebook_token Facebook 유저 id
  *
  * @apiSuccessExample {json} Success-Response:
  HTTP/1.1 200 OK
